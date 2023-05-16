4480df6a189dfd9af3d2a6c870a1aa0e18615633
Karl Schultz <karl.w.schultz@gmail.com>
2023-05-16 07:37:18 -0600

    Minor maintenance fixes

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@
 const int WIDTH = 256;
 const int HEIGHT = 256;
 
-const std::vector<const char*> validationLayers = {"VK_LAYER_LUNARG_standard_validation"};
+const std::vector<const char*> validationLayers = {"VK_LAYER_KHRONOS_validation"};
 
 const std::vector<const char*> deviceExtensions = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
 
